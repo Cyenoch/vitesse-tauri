41d74670669e7a506298163635ccdf5fd82e0eee
Cyenoch <cyenoch@outlook.com>
2024-01-08 22:52:45 +0800

    initial

---
+++ src/components/Greet.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { name } = defineProps<{ name: string }>()
+const fromRust = useObservable(from(invoke('greet', {
+  name,
+})))
+</script>
+
+<template>
+  <div center col h-dvh>
+    <h2>
+      {{ fromRust }}
+    </h2>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
